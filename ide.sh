e68d5fc8ef4a6aba060974a3238793430b4dd7fc
mvs2005 <48253500+mvs2005@users.noreply.github.com>
2020-06-04 21:19:44 +0400

    Update ide.sh

--- ide.sh
+++ ide.sh
@@ -1,10 +1,19 @@
 while read var value
 do
     export "$var"="$value"
-done < ~/vim-ide/ide.conf
+done < ~/vim-ide/workspaces/$2.conf
 
-echo "$server"
-echo "$user"
-echo "$passwd"
-echo "$workDir"
-echo "$sourceDir"
+if [ "$1" = "--connect" ]; then
+    /bin/TEE/tf workspace -new -collection:$server $2
+    /bin/TEE/tf workfold -map -workspace:$2 $sourcedir $workdir
+fi
+
+if [ "$1" = "--get" ]; then
+    cd $HOME/$workdir
+    /bin/TEE/tf get -login:$user,$passwd -force
+fi
+
+if [ "$1" = "-getoverwrite" ]; then
+    cd $HOME/$workdir
+    /bin/TEE/tf get -login:$user,$passwd -force -overwrite
+fi
