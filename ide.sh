43e104e630fa80ca39218228115b2283e7cdd043
mvs2005 <48253500+mvs2005@users.noreply.github.com>
2020-06-09 10:39:14 +0400

    Update ide.sh

--- ide.sh
+++ ide.sh
@@ -8,7 +8,6 @@ fi
 while read var value
 do
     if [ "$var" != "" ]; then
-#        echo "$var=$value"
         export "$var"="$value"
     fi
 done < ~/vim-ide/workspaces/$2.conf
@@ -19,16 +18,26 @@ case $sourcecontroltype in
             get )
                 mkdir -p $HOME/$workdir
                 cd $HOME/$workdir
-                /bin/TEE/tf get -login:$user,$passwd -force
+                /bin/TEE/tf get -login:$user,$passwd -force -overwrite
                 ;;
             connect )
                     /bin/TEE/tf workspace -new -collection:$server $2 -login:$user,$passwd
-                    /bin/TEE/tf workfold -map -workspace:$2 $sourcedir $workdir -login:$user,$passwd
+                    /bin/TEE/tf workfold -map -workspace:$2 $sourcedir $HOME/$workdir -login:$user,$passwd
                 ;;
             open )
+#                echo
+                export "vimWindow"= "cd  ~$workdir && vim"
+                export "scWindow"="alias build='$buildcmd' && alias run='$runcmd'"
                 export TERM='screen-256color'
-                tmux new-session -A -s ide_main \; new-window -n $2 \; split-window -h -p 20 -t "$2.1" \; split-window -v -p 20 -t "$2.1" \; send-keys -t "$2.1" 'cd    ~'$workdir' && vim' Enter \; send-keys -t "$2.3" C-z ' alias build="'$buildcmd'" && alias run="'$runcmd'"' Enter
-                    ;;
+                export "foundWindow"="'$(tmux list-windows -t ide_main | grep $2)'"
+                if [ "$foundWindow" = "''" ]; then
+                    tmux new-session -A -s ide_main \; new-window -n $2 \; split-window -h -p 20 -t "$2.1" \; split-window -v -p 20 -t "$2.1" \; send-keys -t "$2.1" C-z " $vimWindow"  Enter \; send-keys -t "$2.3" C-z " $scWindow" Enter
+                else
+                    tmux new-session -A -s ide_main
+                fi
+                ;;
+            test )
+                ;;
             attach )
                     tmux new-session -A -s ide_main
                 ;;
