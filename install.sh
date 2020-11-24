afa969ee0b69106504227c849facff126b3cc128
mvs2005 <48253500+mvs2005@users.noreply.github.com>
2020-11-24 14:53:40 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,11 +41,37 @@ tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.tmux/plugins"
 "$HOME"/.tmux/plugins/tpm/bin/install_plugins || true
 tmux kill-session -t __noop >/dev/null 2>&1 || true
 
-if [ ! -f "$HOME"/.vim/bundle/Vundle.vim ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git "$HOME"/.vim/bundle/Vundle.vim
-  touch "$HOME"/.vimrc
-  ln -sf ~/vim-ide/vim/vimrc "$HOME"/.vimrc;
-fi
+apt-get install software-properties-common
+add-apt-repository ppa:deadsnakes/ppa 
+apt-get update
+apt-get install -y python3.8 python3.8-dev vim-gtk git gcc libncurses5-dev libncursesw5-dev
+cd /tmp && git clone https://github.com/vim/vim.git
+cd /tmp/vim
+make clean distclean
+" ls -lh /usr/bin/ | grep python3
+" ls /usr/lib/python3.8/config-
+sudo ./configure --enable-python3interp --with-python3-command=/usr/bin/python3.8 -with-python3-config-dir=/usr/lib/python3.8/config-3.8-x86_64-linux-gnu
+make && sudo make install
+
+sudo ln -s /usr/bin/python3.8 /usr/bin/python3
+  
+vim --version
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+apt-key adv --keyserver pgp.mit.edu --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" > /etc/apt/sources.list.d/mono-xamarin.list
+apt-get update
+apt-get install mono-devel
+
+" Install netcoredbg
+mkdir ~/netcoredbg/bin/
+wget https://github.com/lextudio/monodevelop.netcoredbg/releases/download/v1.0/netcoredbg.linux.zip
+unzip ~/netcoredbg/bin/netcoredbg.linux.zip
+rm -rf ~/netcoredbg/bin/netcoredbg.linux.zip
+ln -s ~/netcoredbg/bin/netcoredbg /usr/bin/netcoredbg
+
 vim +PluginInstall +qall
 
 
